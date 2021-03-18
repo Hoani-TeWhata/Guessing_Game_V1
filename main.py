f05edbf5ba4cf7a6297abaa0b738284ad3be070b
Hoani Te <16200@jhc.school.nz>
2021-03-18 23:01:44 +0000

    Added Loop to the code

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ target= random.randint(1,100)
 
 guess = int(input("Please guess the number: "))
 
+while guess != target: 
 
-if guess == target:
-  print("Congratulations! You got the number")
-elif guess < target:
+ 
+ if guess < target:
   print("Your guess was too low")
-elif guess > target:
+ elif guess > target:
   print("Your guess was too high")
-else:
-  print("Something went wrong")  
+ guess = int(input("Please guess the number: "))
+
+
+
+print("Congratulations! You got the number!")
